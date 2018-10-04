8b2615c2e169c1613bfe49f7db37ae1f957e776e
safe <krishna.s@lucideustech.com>
2018-10-04 12:24:09 +0530

    post view,delete,create

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,9 @@ const passport = require("passport");
 //Post model
 const Post = require("../../models/Post");
 
+//Profile model
+const Profile = require("../../models/Profile");
+
 //Validate Post
 const validatePostInput = require("../../validation/post");
 
@@ -13,6 +16,28 @@ const validatePostInput = require("../../validation/post");
 //@desc tests posts route
 //@access public
 router.get("/test", (req, res) => res.json({ msg: "posts works" }));
+
+//@route GET api/posts
+//@desc Get Posts
+//@access public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ noPostsFound: "No posts found." }));
+});
+
+//@route GET api/posts/:id
+//@desc Get Posts by id
+//@access public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .sort({ date: -1 })
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ noPostFound: "No Post Found With That Id " })
+    );
+});
 
 //@route POST api/posts
 //@desc Create Posts
@@ -35,6 +60,29 @@ router.post(
       user: req.user.id
     });
     newPost.save().then(post => res.json(post));
+  }
+);
+
+//@route DELETE api/posts/:id
+//@desc Delete Posts
+//@access private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ unauthorized: "user is unauthorized" });
+          }
+          //Delete to post
+          post.remove().then(() => res.status(200).json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
   }
 );
 
