aaf9cd592347e7ca4e227b3afe565543b8920ce2
krishna21august <krishnakant.singh.min12@gmail.com>
2018-10-06 21:41:59 +0530

    completed static landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
 import "./App.css";
 
 class App extends Component {
@@ -7,6 +9,8 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
