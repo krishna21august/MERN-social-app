00f404c9b0be486e678a80f273c5481c11cc343f
krishna21august <krishnakant.singh.min12@gmail.com>
2018-10-14 13:02:52 +0530

    added login with redux and validation

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-
-import { GET_ERRORS } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import jwt_decode from "jwt-decode";
 //Register User
 
 export const registerUser = (userData, history) => dispatch => {
@@ -13,4 +14,39 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//Get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      //Save to local Storage
+      const { token } = res.data;
+      //set token to ls
+      localStorage.setItem("jwtToken", token);
+      //set token to auth Header
+      setAuthToken(token);
+
+      //decode auth token to get user id
+      const decoded = jwt_decode(token);
+
+      //Set current user
+      dispatch(setCurrentUSer(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Set logged in user
+
+const setCurrentUSer = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
---
+++ client/src/validation/is-empty.js
@@ -0,0 +1,10 @@
+//new convention of making functions asper ES6 modules.
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+//Export function isEmpty.
+//I can import this function using import from any file and use it
+export default isEmpty;
