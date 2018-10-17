72857eef921fbb04a0593dbcc195d07c23ac25be
safe <krishna.s@lucideustech.com>
2018-10-17 12:54:51 +0530

    get profile using reducer

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import classnames from "classnames";
 import TextFieldGroup from "../common/TextFieldGroup";
 import { loginUser } from "../../actions/authActions";
 class Login extends Component {
