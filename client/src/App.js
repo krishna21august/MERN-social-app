1c833248ef49c9f21c7b0a3d1abe43aed37d7f38
safe <krishna.s@lucideustech.com>
2018-10-18 19:42:07 +0530

    added form components

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
 
 import "./App.css";
 
@@ -50,6 +51,14 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
+                CreateProfile
               </Switch>
             </div>
             <Footer />
---
+++ client/src/components/common/InputGroup.js
@@ -0,0 +1,48 @@
+import React from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+const InputGroup = ({
+  name,
+  placeholder,
+  value,
+  error,
+  icon,
+  type,
+  onChange
+}) => {
+  return (
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text">
+          <i className={icon} />
+        </span>
+      </div>
+      <input
+        className={classnames("form-control form-control-lg", {
+          "is-invalid": error
+        })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+InputGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  icon: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  error: PropTypes.string
+};
+InputGroup.defaultProps = {
+  type: "text"
+};
+
+export default InputGroup;
