ebe47f9986bd703029b6ac698220cebeb152ba23
krishna21august <krishnakant.singh.min12@gmail.com>
2018-10-18 10:59:43 +0530

    changes fr profile

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,6 +3,7 @@ import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 import profileReducer from "./profileReducer";
 
+//export the state
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
