501abda8f713fc5d8c9cf5c464d3ecec1cc69bc8
safe <krishna.s@lucideustech.com>
2018-10-21 14:50:25 +0530

    add eduaction

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import Spinner from "../common/Spinner";
 import { Link } from "react-router-dom";
 import ProfileActions from "./ProfileActions";
 import Experience from "./Experience";
+import Education from "./Education";
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -35,6 +36,8 @@ class Dashboard extends Component {
             {/*Now Experience component can acccess {profile.experience}
           as property and we can access it as this.props.experience*/}
             <Experience experience={profile.experience} />
+            <Education education={profile.education} />
+
             <div style={{ marginBottom: "60px" }} />
             <button
               onClick={this.onDeleteClick.bind(this)}
