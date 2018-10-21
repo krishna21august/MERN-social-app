791024c82fe070e7f39f1cc0d37a37e43a8174fd
krishna21august <krishnakant.singh.min12@gmail.com>
2018-10-21 14:50:25 +0530

    add eduaction

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -112,3 +112,21 @@ export const deleteExperience = id => dispatch => {
       })
     );
 };
+
+// Delete Education
+export const deleteEducation = id => dispatch => {
+  axios
+    .delete(`/api/profile/education/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
