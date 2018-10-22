27f9e69bae77e0cee9a3775d1e48aaa37a7972f1
safe <krishna.s@lucideustech.com>
2018-10-22 18:31:01 +0530

    add posts

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { connect } from "react-router-dom";
+import PropTypes from "prop-types";
+import PostForm from "./PostForm";
+import Spinner from "../common/Spinner";
+
+class Posts extends Component {
+  render() {
+    return (
+      <div className="feed">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <PostForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Posts;
