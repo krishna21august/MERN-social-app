48001c4524d9cba1f9b30704362877476be29eae
safe <krishna.s@lucideustech.com>
2018-10-25 13:33:43 +0530

    config keys for connections

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-/*Used to export the object so that it can be accessed from outer scope.
-If you require this module then you have to export module using module.exports*/
-
-module.exports = {
-  mongoURI: "mongodb://krishna:krishna123@ds215172.mlab.com:15172/merndb",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
